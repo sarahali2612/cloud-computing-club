d2b5d7a26f889fe78bd41454b19c5023c178378d
sarahali2612 <syedsarah1205@gmail.com>
2026-08-26 18:13:01 +0530

    Connect website to Supabase

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const SUPABASE_URL = "PASTE_YOUR_API_URL_HERE";uezxymkecrdmixopybxq.supabase.co
 const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";sb_publishable_qtktn0Q4hNZkrOCj8nYnpg_g6OYdHKo
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
 // Welcome message
 console.log("Galgotias Cloud Computing Club loaded! ☁️");
 
